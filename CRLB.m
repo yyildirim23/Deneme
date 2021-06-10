2edcc301b3dd8dcabb20a8a55e59c3bf95428269
yyildirim23 <40305316+yyildirim23@users.noreply.github.com>
2021-06-10 20:04:49 +0300

    Update CRLB.m

--- CRLB.m
+++ CRLB.m
@@ -11,9 +11,11 @@ y = diff(logGaussian,A);
 z = diff(-y,A);
 
 
-A = 0;   fs =100; x=-5:1/fs:5-1/fs; var=1;
+A = 0;   fs =100; x=-5:1/fs:5-1/fs; var=2;
 
 m = double(z(A,var,x));
 
 
-plot(x(1:end),m)
+plot(x,m)
+figure(101)
+plot(x,double(Gaussian(A,var,x)))
